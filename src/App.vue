ec640acc9e67ea838afbc4225a7b339c36591470
zhangyuhui <zhangyuhui755@163.com>
2019-06-13 15:23:29 +0800

    message 123

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>123</div>
+    <div>456</div>
   </div>
 </template>
 
